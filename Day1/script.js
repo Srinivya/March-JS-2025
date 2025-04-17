9a936382ed5d416ecb00aca3f16d0b1121aafcb0
Srinivya <srinivyat@gmail.com>
2025-04-17 17:31:45 +0530

    Initial commit

---
+++ Day1/script.js
@@ -0,0 +1,66 @@
+// console.log("hii");
+// let a = 10;
+
+// let b = 10;
+// console.log(a + b);
+
+// a = 20;
+// let a=90;
+// let
+// reassign
+
+// const
+// reassign no
+// reinnitizilise
+// no change
+
+// let -block scope
+let apple = 10;
+let orange = 10;
+
+orange = 20;
+// let apple=90;
+document.writeln(orange);
+
+const fixed = 100;
+// fixed = 110;
+console.log(fixed);
+
+// var-global scope
+var age1 = 20;
+// console.log(age1);
+
+var age1 = 21;
+
+console.log(age1);
+
+let decim = 10.9;
+
+let name = "sri";
+document.writeln(name);
+
+var bool = true;
+
+let fruit;
+
+console.log(fruit);
+
+var type = null;
+console.log(type);
+
+let cars = ["BMW", "volvo", "scoda"];
+console.log(cars.length);
+
+//    0     1          2
+
+console.log(cars);
+console.log(cars[0]);
+
+let person = {
+  name: "Dharani",
+  age: 20,
+  
+};
+
+console.log(person.age);
+console.log(person.name);
